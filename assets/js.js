fe3525ed6ad9a421e04e8154589689da2142c908
Margot Cooper Gruen <margotgruen@gmail.com>
2022-07-26 21:09:10 -0700

    linked files to each other, added basics in html, links in JS

--- assets/js.js
+++ assets/js.js
@@ -1,1 +1,20 @@
 //link to API for weather: https://openweathermap.org/api/one-call-api#how
+//link on how to get current weather data: https://openweathermap.org/current
+//link on how to get 5 day forecast: https://openweathermap.org/forecast5
+
+// window.localStorage.setItem("city");
+
+//
+const city = document.getElementById("city");
+const btn = document.getElementById("btn");
+const cityResults = document.getElementById("cityResults");
+
+btn.onclick = function () {
+  const location = city.value;
+
+  console.log(city);
+
+  if (city !== null) {
+    //
+  }
+};
